8a11f6e53bee7430b5b4e7c28a8dddf714c9063b
Lawson Matutu <lawsonmatutu@gmail.com>
2024-10-22 08:22:03 -0700

    updating google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/landing/Navbar';
 import FooterTop from './components/footer/FooterTop';
@@ -6,10 +6,18 @@ import FooterBottom from './components/footer/FooterBottom';
 import Landing from './components/landing/Landing';
 import Contact from './components/Contact';
 import TopNavbar from './components/landing/TopNavbar';
+import ReactGA from 'react-ga'; // Import ReactGA
 
+const TRACKING_ID = "G-BKGFJFK34S"; // Replace with your Google Analytics Tracking ID
 
 
 const App: React.FC = () => {
+
+
+  useEffect(() => {
+    ReactGA.initialize(TRACKING_ID); // Initialize GA
+    ReactGA.pageview(window.location.pathname + window.location.search); // Record initial page view
+  }, []);
   return (
     <>
     
